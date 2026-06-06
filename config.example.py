b38ace280d17c4d5c539c4c5ff9298bd2de2ee18
howellanthony-lang <howellanthony@icloud.com>
2026-06-06 15:01:13 +0100

    Add safe config example

---
+++ config.example.py
@@ -0,0 +1,17 @@
+SCHEDULE_WEBHOOK = "https://discord.com/api/webhooks/YOUR_SCHEDULE_WEBHOOK_HERE"
+LIVE_TIMING_WEBHOOK = "https://discord.com/api/webhooks/YOUR_LIVE_TIMING_WEBHOOK_HERE"
+FASTEST_LAP_WEBHOOK = "https://discord.com/api/webhooks/YOUR_FASTEST_LAP_WEBHOOK_HERE"
+RACE_CONTROL_WEBHOOK = "https://discord.com/api/webhooks/YOUR_RACE_CONTROL_WEBHOOK_HERE"
+NEWS_WEBHOOK = "https://discord.com/api/webhooks/YOUR_NEWS_WEBHOOK_HERE"
+WEATHER_WEBHOOK = "https://discord.com/api/webhooks/YOUR_WEATHER_WEBHOOK_HERE"
+STANDINGS_WEBHOOK = "https://discord.com/api/webhooks/YOUR_STANDINGS_WEBHOOK_HERE"
+RESULTS_WEBHOOK = "https://discord.com/api/webhooks/YOUR_RESULTS_WEBHOOK_HERE"
+
+NEWS_FEEDS = [
+    "https://www.formula1.com/en/latest/all.xml",
+    "https://www.motorsport.com/rss/f1/news/",
+]
+
+SEASON = 2026
+NEWS_LIMIT = 5
+STANDINGS_LIMIT = 10
